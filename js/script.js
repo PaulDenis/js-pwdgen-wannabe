536b2196f351a2a1421aaa775ae0a41bdc994198
PaulDenis <d.paul@outlook.it>
2021-04-26 14:26:23 +0200

    Richiesta colore completa

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ console.log(nome);
 
 var cognome = prompt("E invece qual'è il tuo cognome?");
 console.log(cognome);
+
+var fav_color = prompt("Adesso vorrei sapere qual'è il tuo colore preferito");
+console.log(fav_color);
